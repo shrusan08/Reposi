45dfd093f0eb5035356dbf469a46f9f00d77a7db
sanke <sanke@DESKTOP-0E8IKFC>
2024-12-27 16:36:16 +0530

    hello

---
+++ Calculator2/src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+	int a=10;
+	int b=5;
+	int c;
+public void add()
+{
+	c=a+b;
+	System.out.println("addition of two numbers is: " +c);
+}
+public void sub()
+{
+	c=a-b;
+	System.out.println("subtraction of two numbers is: " +c);
+}
+
+}
---
+++ Calculator2/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void main(String[] args)
+	{
+	Calculator cal=new Calculator();
+	cal.add();
+	cal.sub();
+	}
+
+
+}
